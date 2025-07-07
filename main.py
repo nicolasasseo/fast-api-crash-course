8d9eb5f0abed742d90238ca68725b1b104069561
nicolasasseo <nicolasasseogithub@gmail.com>
2025-07-07 15:51:55 +0200

    Added put & delete endpoints

--- main.py
+++ main.py
@@ -45,3 +45,34 @@ def get_todos(first_n: int = 0):
     if first_n > 0:
         return {"result": all_todos[:first_n]}
     return {"result": all_todos}
+
+
+@api.post("/todos")
+def create_todo(todo: dict):
+    new_todo_id = len(all_todos) + 1
+    new_todo = {
+        "todo_id": new_todo_id,
+        "todo_name": todo["todo_name"],
+        "todo_description": todo["todo_description"],
+    }
+    all_todos.append(new_todo)
+    return {"added new todo": new_todo}
+
+
+@api.put("/todos/{todo_id}")
+def update_todo(todo_id: int, updated_todo: dict):
+    for todo in all_todos:
+        if todo["todo_id"] == todo_id:
+            todo["todo_name"] = updated_todo["todo_name"]
+            todo["todo_description"] = updated_todo["todo_description"]
+            return {"updated todo": todo}
+    return {"result": "Todo not found"}
+
+
+@api.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int):
+    for todo in all_todos:
+        if todo["todo_id"] == todo_id:
+            all_todos.remove(todo)
+            return {"result": "Todo deleted"}
+    return {"result": "Todo not found"}
