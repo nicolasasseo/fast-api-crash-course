c6af8831ab726aceb706ce1647285543fc12d8a0
nicolasasseo <nicolasasseogithub@gmail.com>
2025-07-07 16:25:47 +0200

    Added schema

--- main.py
+++ main.py
@@ -1,28 +1,70 @@
 from fastapi import FastAPI
+from typing import List, Optional
+from enum import IntEnum
+from pydantic import BaseModel, Field
+
+
+class Priority(IntEnum):
+    LOW = 3
+    MEDIUM = 2
+    HIGH = 1
+
+
+class TodoBase(BaseModel):
+    todo_name: str = Field(
+        ..., min_length=3, max_length=512, description="The name of the todo"
+    )
+    todo_description: str = Field(..., description="The description of the todo")
+    priority: Priority = Field(
+        default=Priority.LOW, description="The priority of the todo"
+    )
+
+
+class TodoCreate(TodoBase):
+    pass
+
+
+class TodoUpdate(BaseModel):
+    todo_name: Optional[str] = Field(
+        None, min_length=3, max_length=512, description="The name of the todo"
+    )
+    todo_description: Optional[str] = Field(
+        None, description="The description of the todo"
+    )
+    priority: Optional[Priority] = Field(None, description="The priority of the todo")
+
+
+class Todo(TodoBase):
+    todo_id: int = Field(..., description="Unique identifier of the todo")
+
 
 api = FastAPI()
 
 all_todos = [
-    {
-        "todo_id": 1,
-        "todo_name": "Buy groceries",
-        "todo_description": "Go to carrefour",
-    },
-    {
-        "todo_id": 2,
-        "todo_name": "Sports",
-        "todo_description": "Go to the gym",
-    },
-    {
-        "todo_id": 3,
-        "todo_name": "Study",
-        "todo_description": "Study for the exam",
-    },
-    {
-        "todo_id": 4,
-        "todo_name": "Read a book",
-        "todo_description": "Read the Lord of the Rings",
-    },
+    Todo(
+        todo_id=1,
+        todo_name="Buy groceries",
+        todo_description="Go to carrefour",
+        priority=Priority.LOW,
+    ),
+    Todo(
+        todo_id=2,
+        todo_name="Sports",
+        todo_description="Go to the gym",
+        priority=Priority.MEDIUM,
+    ),
+    Todo(
+        todo_id=3,
+        todo_name="Study",
+        todo_description="Study for the exam",
+        priority=Priority.HIGH,
+    ),
+    Todo(
+        todo_id=4,
+        todo_name="Read a book",
+        todo_description="Read the Lord of the Rings",
+        priority=Priority.LOW,
+    ),
 ]
 
 
@@ -32,47 +74,52 @@ def index():
     return {"message": "Hello World"}
 
 
-@api.get("/todos{todo_id}")
+@api.get("/todos{todo_id}", response_model=Todo)
 def get_todo(todo_id: int):
     for todo in all_todos:
-        if todo["todo_id"] == todo_id:
-            return {"result": todo}
+        if todo.todo_id == todo_id:
+            return todo
     return {"result": "Todo not found"}
 
 
-@api.get("/todos")
+@api.get("/todos", response_model=List[Todo])
 def get_todos(first_n: int = 0):
     if first_n > 0:
         return {"result": all_todos[:first_n]}
     return {"result": all_todos}
 
 
-@api.post("/todos")
-def create_todo(todo: dict):
+@api.post("/todos", response_model=Todo)
+def create_todo(todo: TodoCreate):
     new_todo_id = len(all_todos) + 1
-    new_todo = {
-        "todo_id": new_todo_id,
-        "todo_name": todo["todo_name"],
-        "todo_description": todo["todo_description"],
-    }
+    new_todo = Todo(
+        todo_id=new_todo_id,
+        todo_name=todo.todo_name,
+        todo_description=todo.todo_description,
+        priority=todo.priority,
+    )
     all_todos.append(new_todo)
     return {"added new todo": new_todo}
 
 
-@api.put("/todos/{todo_id}")
-def update_todo(todo_id: int, updated_todo: dict):
+@api.put("/todos/{todo_id}", response_model=Todo)
+def update_todo(todo_id: int, updated_todo: TodoUpdate):
     for todo in all_todos:
-        if todo["todo_id"] == todo_id:
-            todo["todo_name"] = updated_todo["todo_name"]
-            todo["todo_description"] = updated_todo["todo_description"]
+        if todo.todo_id == todo_id:
+            if updated_todo.todo_name is not None:
+                todo.todo_name = updated_todo.todo_name
+            if updated_todo.todo_description is not None:
+                todo.todo_description = updated_todo.todo_description
+            if updated_todo.priority is not None:
+                todo.priority = updated_todo.priority
             return {"updated todo": todo}
     return {"result": "Todo not found"}
 
 
-@api.delete("/todos/{todo_id}")
+@api.delete("/todos/{todo_id}", response_model=Todo)
 def delete_todo(todo_id: int):
     for todo in all_todos:
-        if todo["todo_id"] == todo_id:
+        if todo.todo_id == todo_id:
             all_todos.remove(todo)
-            return {"result": "Todo deleted"}
+            return {"deleted todo": todo}
     return {"result": "Todo not found"}
